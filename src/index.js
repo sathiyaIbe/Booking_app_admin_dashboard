f229d01aebeff46f776c60c95852494c3603b551
sathiyaIbe <sathiyaibe@gmail.com>
2023-05-16 17:19:54 +0530

    16-05-23

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import 'primereact/resources/themes/md-light-indigo/theme.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
